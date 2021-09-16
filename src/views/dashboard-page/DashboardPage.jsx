81184d6d61535ff160400e3679416d27f5dc73cd
swarup-e <swarup@datakaveri.org>
2021-09-16 10:38:32 +0530

    Updated server list page

--- src/views/dashboard-page/DashboardPage.jsx
+++ src/views/dashboard-page/DashboardPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import { Table, Button } from 'reactstrap';
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
+import moment from 'moment';
 
 import NavBar from 'views/shared/nav-bar/NavBar';
 import ServerAction from 'stores/servers/ServerAction';
@@ -27,17 +29,53 @@ const Dashboard = (props) => {
         }
     }
 
+    const formatDate = (datestring) => {
+        return moment(datestring).format('LLL');
+    }
+
     return (
         <div>
             <NavBar />
 
             <div className="server-container">
 
-                <div className="header">
-                    <p className="title">Find cameras by registered servers</p>
+                <div className="table-header">
+                    <h4 className="title">Registered Servers</h4>
                 </div>
+                <div className="content-border" />
+                <div className="table-container">
+                    {
+                        Array.isArray(registeredServers) && registeredServers.length > 0 ? (
 
-                <div className="server-cards">
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Server Name</th>
+                                        <th>Last Active Time</th>
+                                        <th>Cameras</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {registeredServers.map((server, i) => (
+                                        <tr key={i}>
+                                            <td>{server.serverName}</td>
+                                            <td>{formatDate(server.lastPingTime)}</td>
+                                            <td>
+                                                <Button color="info" onClick={(e) => handleServerClick(e, server.serverId)}>
+                                                    Show Cameras
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        ) :
+                            <h5>No servers to display</h5>
+                    }
+                </div>
+                
+
+                {/* <div className="server-cards">
                     {
                         Array.isArray(registeredServers) && registeredServers.length > 0 ?
                             registeredServers.map((server, i) => (
@@ -51,7 +89,7 @@ const Dashboard = (props) => {
                             ))
                             : <h5>No servers to display</h5>
                     }
-                </div>
+                </div> */}
             </div>
         </div >
     );
